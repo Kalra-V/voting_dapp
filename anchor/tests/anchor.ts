6550070d6476a583d8a4c7bb75d6e2241bfe8b51
Vansh Kalra <vansh@erino.io>
2025-11-27 17:41:42 +0530

    Added test for initializing poll

--- anchor/tests/anchor.ts
+++ anchor/tests/anchor.ts
@@ -1,16 +1,41 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Anchor } from "../target/types/anchor";
+import { PublicKey } from "@solana/web3.js";
+import { BankrunProvider, startAnchor } from "anchor-bankrun";
+import { expect } from "chai";
 
-describe("anchor", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+const IDL = require("../target/idl/anchor.json");
 
-  const program = anchor.workspace.anchor as Program<Anchor>;
+const votingAddress = new PublicKey("FyUxZy67eUenJwRsucA7iJozGLSFJf3xzq5hR6SXrtUv")
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+describe("Anchor", () => {
+  it("Initialize Poll!", async () => {
+    const context = await startAnchor("", [{ name: "anchor", programId: votingAddress }], []);
+    const provider = new BankrunProvider(context);
+
+    const votingProgram = new Program<Anchor>(IDL, provider);
+
+    await votingProgram.methods.initializePoll(
+      new anchor.BN(1),
+      new anchor.BN(0),
+      new anchor.BN(1864244339),
+      "Which is your favorite programming language?"
+    ).rpc();
+
+    const [pollAddress] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, "le", 8)],
+      votingAddress
+    );
+
+    const poll = await votingProgram.account.poll.fetch(pollAddress);
+
+    console.log("Poll:", poll);
+
+    expect(poll.pollId.toNumber()).to.equal(1);
+    expect(poll.description).to.equal("Which is your favorite programming language?");
+    expect(poll.pollStart.toNumber()).to.be.lessThan(poll.pollEnd.toNumber());
+
+    console.log("Poll initialized!");
   });
 });
